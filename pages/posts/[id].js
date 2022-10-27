8eb8d273708e8323bedbd037228bf31dc4ff0a86
denis.korniychuk <denis.korniychuk@pm.bet>
2022-10-27 08:22:05 +0300

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <br />
+            <br />
+            <br />
+            <Date dateString={postData.date} />
+        </Layout>
+    );
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+
+export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
